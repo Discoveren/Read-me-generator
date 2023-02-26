3b82ac6aee542196e954c2337291f631ca128c81
Discoveren <renniel.pena@gmail.com>
2023-02-26 23:29:31 +0000

    Added questions for the readme

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+{
+    type: 'input',
+    name:'title',
+    message: 'Please enter a title for your project' 
+},
+{
+    type: 'editor',
+    name:'description',
+    message: 'Please enter a description for your project'
+},
+{
+    type: 'input',
+    name:'table of Contents',
+    message: 'Please enter a table of contents for your project'
+},
+{
+    type: 'input',
+    name:'installation',
+    message: 'Please enter some installation instructions for your project'
+},
+{
+    type: 'input',
+    name:'usage',
+    message: 'Please enter a usage for your project'
+},
+{
+    type: 'list',
+    name:'license',
+    message: 'What License should this project fall under?',
+    choices: ['Harvard', 'MIT', 'ISC']
+},    
 
 ];
 
